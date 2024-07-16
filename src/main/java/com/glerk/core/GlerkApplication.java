5d18a64cb16874651842b95a6af4c9742fd3ec88
Jieun Oh <ohjieun@kakao.com>
2024-07-16 11:07:52 +0900

    Setting: 프로젝트 기본 설정 완료

---
+++ src/main/java/com/glerk/core/GlerkApplication.java
@@ -0,0 +1,13 @@
+package com.glerk.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlerkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlerkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glerk/core/GlerkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glerk.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlerkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
