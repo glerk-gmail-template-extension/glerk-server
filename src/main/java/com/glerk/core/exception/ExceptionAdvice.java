078982c5be04000c07065ddf0f1cee902a722d1b
Jieun Oh <ohjieun@kakao.com>
2024-07-21 01:28:05 +0900

    Style: 불필요한 세미콜론 제거

--- src/main/java/com/glerk/core/exception/ExceptionAdvice.java
+++ src/main/java/com/glerk/core/exception/ExceptionAdvice.java
@@ -1,6 +1,6 @@
 package com.glerk.core.exception;
 
-import org.springframework.http.ResponseEntity;;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
