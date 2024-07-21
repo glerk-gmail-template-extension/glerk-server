dc1931d0a0e7596d8d985bd2933da04f9140daf8
Jieun Oh <ohjieun@kakao.com>
2024-07-21 17:00:19 +0900

    Style: 사용 안하는 ErrorCode 정리 및 메시지 수정

--- src/main/java/com/glerk/core/exception/ErrorCode.java
+++ src/main/java/com/glerk/core/exception/ErrorCode.java
@@ -7,13 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "Invalid email format"),
-    REGISTRATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Registration failed"),
-    SECURITY_EXCEPTION(HttpStatus.UNAUTHORIZED, "Security issue occurred during authentication"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
-    USER_ALREADY_EXIST(HttpStatus.CONFLICT, "User already exists"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
-    GOOGLE_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "Google user not found");
+    REGISTRATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Registration failed. \nPlease try again later."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token."),
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT, "User already exists. \nPlease log in."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found. \nPlease sign up."),
+    GOOGLE_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "Google user not found.");
 
     private final HttpStatus httpStatus;
     private final String message;
